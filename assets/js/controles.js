c9be85277584ba6c9a24ec564ab4fdeed17179ce
Patricia Romero <patricia.romarquez@gmail.com>
2023-01-29 20:51:29 +0100

    entrega practica tema 6

---
+++ assets/js/controles.js
@@ -0,0 +1,83 @@
+function accionPlay()
+{
+	if(!medio.paused && !medio.ended) 
+	{
+		medio.pause();
+		play.value='\u25BA'; //\u25BA
+    document.body.style.backgroundColor = '#fff';
+	}
+	else
+	{
+		medio.play();
+		play.value='||';
+    document.body.style.backgroundColor = 'grey';
+	}
+}
+function accionReiniciar()
+{
+    medio.currentTime = 0;
+    medio.play();
+	play.value='||';
+
+}
+function accionRetrasar()
+{
+   if(medio.currentTime > 0){
+    medio.currentTime -= 10;
+   }
+	//Usar propiedad .curentTime
+  //Contemplar que no existen valores negativos
+}
+function accionAdelantar()
+{
+    if(medio.currentTime < medio.duration){
+    medio.currentTime += 10;
+    }
+    
+  //Contemplar que no existen valores mayores a medio.duration	
+}
+function accionSilenciar()
+{
+    if(!medio.muted) 
+	{
+		medio.muted = true;
+	}
+	else
+	{
+		medio.muted = false;
+	}
+}
+function accionMasVolumen()
+{
+	if(medio.volume >= 0 && medio.volume < 1) {
+        medio.volume = medio.volume + 0.1;
+    }
+}
+function accionMenosVolumen()
+{
+    if(medio.volume > 0 && medio.volume <= 1) {
+        medio.volume = medio.volume - 0.1;
+    }
+	//Contemplar que el valor mínimo de volumen es 0
+}
+
+function iniciar() 
+{
+	
+	medio=document.getElementById('medio');
+	play=document.getElementById('play');
+	reiniciar=document.getElementById('reiniciar');
+	retrasar=document.getElementById('retrasar');
+	adelantar=document.getElementById('adelantar');
+	silenciar=document.getElementById('silenciar');
+
+	play.addEventListener('click', accionPlay);
+	reiniciar.addEventListener('click', accionReiniciar);
+	retrasar.addEventListener('click', accionRetrasar);
+	adelantar.addEventListener('click', accionAdelantar);
+	silenciar.addEventListener('click', accionSilenciar);
+	menosVolumen.addEventListener('click', accionMenosVolumen);
+	masVolumen.addEventListener('click', accionMasVolumen);
+}
+
+window.addEventListener('load', iniciar, false);
